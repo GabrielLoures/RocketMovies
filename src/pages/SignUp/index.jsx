207de7a8a4faa68d5f193a366fa0415929cb2fd7
Gabriel <gabrielloures21@gmail.com>
2022-10-18 15:53:43 -0300

    End SignUp

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,38 @@
+import { Container, Background, Form } from './styles';
+
+import { HiOutlineMail, HiOutlineLockClosed, HiOutlineUser, HiOutlineArrowSmLeft } from 'react-icons/hi';
+
+import { Input } from '../../components/Input';
+
+import { Button } from '../../components/Button';
+
+export function SignUp() {
+
+  return(
+    <Container>
+
+      <Form>
+
+        <h1>RocketMovies</h1>
+        <span>Aplicação para acompanhar tudo que assistir.</span>
+
+        <h2>Crie sua Conta</h2>
+        
+        <Input placeholder="Nome" type="text" icon={HiOutlineUser} />
+        <Input placeholder="E-mail" type="text" icon={HiOutlineMail} />
+        <Input placeholder="Senha" type="passaword" icon={HiOutlineLockClosed} />
+
+        <Button title="Cadastrar" />
+
+        <a href="#">
+          <HiOutlineArrowSmLeft />
+          Voltar para o login
+        </a>
+      </Form>
+    
+      <Background></Background>
+
+    </Container>
+  )
+  
+}
