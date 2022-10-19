a7d91e8fd0b157fa3df2631dfeef96cc4cb66370
Gabriel <gabrielloures21@gmail.com>
2022-10-19 11:08:26 -0300

    End MoviePreview

--- src/pages/MoviePreview/styles.js
+++ src/pages/MoviePreview/styles.js
@@ -5,16 +5,24 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
 
-  > div {
+  > main {
     padding: 40px 123px;
   }
 
-  > div a { 
+  > main a { 
     color: ${({ theme }) => theme.COLORS.PINK_100};
     font-size: 20px;
     display: flex;
     align-items: center;
     gap: 8px;
+  }
+
+  > main p {
+    color: ${({ theme }) => theme.COLORS.TEXT_COLOR_WHITE};
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 21px;
+    margin-top: 40px;
   }
 
 `;
